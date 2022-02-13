2690a2fb7e90a382783f91fd1b7c89494e88f9c8
Alto1772 <56553686+Alto1772@users.noreply.github.com>
2022-02-13 18:58:12 +0800

    added template mcworld and map overlay

--- pw52mc.py
+++ pw52mc.py
@@ -2,4 +2,10 @@
 import pw5conv
 import sys
 
-pw5conv.save_to_mc_world(sys.argv[2], pw5conv.load_pw5_map(sys.argv[1]))
+overlay = False
+for i in range(1, len(sys.argv)):
+    if sys.argv[i] == '-o':
+        overlay = True
+        sys.argv.pop(i)
+
+pw5conv.save_to_mc_world(sys.argv[2], pw5conv.load_pw5_map(sys.argv[1]), overlay=overlay)
